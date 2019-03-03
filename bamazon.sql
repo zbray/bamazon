9ea8d693aae7bbe9e473c9ec7eda34d906dba0dc
Cold Zebra <zachdbray@gmail.com>
2019-03-03 17:06:36 -0600

    working version 1

--- bamazon.sql
+++ bamazon.sql
@@ -13,15 +13,15 @@ CREATE TABLE products (
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES 
-    ("master sword", "weapons", 99.9, 1), 
-    ("sword of the six sages", "weapons", 49.9, 5), 
-    ("green tunic", "clothing", 22.5, 10), 
-    ("green hat", "clothing", 14.9, 5), 
-    ("hookshot", "weapons", 29.9, 15), 
-    ("bow", "weapons", 34.9, 15), 
-    ("arrows", "ammo", 4.9, 99), 
-    ("bombs", "ammo", 19.9, 99), 
-    ("brown boots", "clothing", 29.9, 20), 
+    ("master sword", "weapons", 99.99, 1), 
+    ("sword of the six sages", "weapons", 49.99, 5), 
+    ("green tunic", "clothing", 22.50, 10), 
+    ("green hat", "clothing", 14.99, 5), 
+    ("hookshot", "weapons", 29.99, 15), 
+    ("bow", "weapons", 34.99, 15), 
+    ("arrows", "ammo", 4.99, 99), 
+    ("bombs", "ammo", 19.99, 99), 
+    ("brown boots", "clothing", 29.99, 20), 
     ("boomerang", "weapons", 9.9, 1);
 
 SELECT * FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,9 +19,12 @@ connection.connect(function (err) {
   initShop();
 });
 
-//Shop Functions
+//init function
 function initShop() {
-  console.log("It's dangerous to go alone! Gear up!");
+  console.log("‌‌▲");
+  console.log("▲‌ ▲");
+  console.log("");
+  console.log("It's dangerous to go alone! Gear up!\n");
   connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw (err);
     for (var i = 0; i < res.length; i++) {
@@ -31,29 +34,53 @@ function initShop() {
   buyItems();
 }
 
+//buy function
 function buyItems() {
   inquirer
     .prompt([{
-        name: "id",
+        name: "purchasedId",
         type: "input",
-        message: "What is the ID of the product you would like to buy?" + "\n",
+        message: "What is the ID of the product you need, young hero?\n\n",
+        filter: Number
       },
       {
-        name: "quantity",
+        name: "purchasedQuant",
         type: "input",
-        message: "How many are you interested in?"
+        message: "And how many will you be needing today?",
+        filter: Number
       }
     ])
+    //Check stock to ensure purchase can occur, update stock and return purchase total
     .then(function (answer) {
-      //Confirm purchase and thank user
-      //Check for stock
+      connection.query("SELECT * FROM products WHERE ?", {
+        item_id: answer.purchasedId
+      }, function (err, res) {
+        if (err) throw (err);
+        if (answer.purchasedQuant <= res[0].stock_quantity) {
+          console.log("Excellent choice, young hero!");
+          connection.query("UPDATE products SET ? WHERE ?",
+            [{
+                stock_quantity: (res[0].stock_quantity - answer.purchasedQuant)
+              },
+              {
+                item_id: answer.purchasedId
+              }
+            ],
+            function (err, res) {
+              if (err) throw (err);
+            })
+          console.log("Ah, perfect. After you've purchased " + answer.purchasedQuant + " You've left me with " + res[0].stock_quantity);
+          console.log("Your total today, young hero, is $" + answer.purchasedQuant * res[0].price + "\nYour rupees are no good here!");
+          orderAgain();
+        } else {
+          console.log("Ah, young hero I seem to be out of that today. Perhaps something else? \n");
+          orderAgain();
+        };
+      });
     });
 }
 
-function updateshop() {
-
-};
-
+//order again function
 function orderAgain() {
   inquirer.prompt([{
     name: "orderAgain",
@@ -64,7 +91,14 @@ function orderAgain() {
     if (answer.orderAgain === "Yes") {
       initShop();
     } else {
-      console.log("Thank you, young hero! Come again.");
+      leaveShop();
     }
   })
 };
+
+//done function
+function leaveShop() {
+  connection.end();
+  console.log("Thank you, young hero! May you bring peace to our land.");
+  console.log("Connection has ended.")
+}
